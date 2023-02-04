b51620444c8cc94aae88f2d6f8853de66b8aa959
Nikita Sokolov <sokolovniki15@yandex.ru>
2023-02-04 18:15:53 +0300

    tyer

---
+++ java/les1/javapracic1/src/main/java/lesssn1/main.java
@@ -0,0 +1,10 @@
+package lesssn1;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите имя: ");
+        String name = scanner.next();
+        System.out.printf("Привет, " + name + "!");
+    }
+}
